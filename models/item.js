77d4a5a646bb13120f73521f55d89bcf0d253213
bnoora <bnooranii@gmail.com>
2024-01-14 12:43:38 -0800

    nodemon install and item model

---
+++ models/item.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new Schema({
+    name: { type: String, required: true, minLenght: 2, maxLenght: 100 },
+    description: { type: String, required: true, minLenght: 2, maxLenght: 100 },
+    price: { type: Number, required: true, min: 0 },
+    stock: { type: Number, required: true, min: 0 },
+    category: { type: Schema.Types.ObjectId, ref: "Category", required: true }
+});
+
+itemSchema.virtual("url").get(function () {
+    return '/item/{this._id}';
+});
+
+
