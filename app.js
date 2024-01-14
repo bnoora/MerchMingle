bc291ad641aaeb8f397f35ddabadda32ec9d8d3f
bnoora <bnooranii@gmail.com>
2024-01-14 14:39:57 -0800

    app setup

--- app.js
+++ app.js
@@ -3,13 +3,47 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+const compression = require('npm install');
+const helmet = require('helmet');
+const RateLimit = require("express-rate-limit");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
 
-// view engine setup
+
+// set up rate limiter: maximum of 20 requests per minute
+const limiter = new RateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minute
+	max: 20
+});
+app.use(limiter);
+
+// Add helmet middleware
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			"script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+		},
+	})
+);
+
+// Set up mongoose connection
+mongoose.set("strictQuery", false);
+const dev_db_url = "mongodb+srv://bnoorani:d5ZsgTgR6g@cluster0.9qjllmq.mongodb.net/inventory_app?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+main().catch((err) => console.log(err));
+async function main() {
+	await mongoose.connect(mongoDB);
+}
+
+
+
+// view engine setup PUG
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -18,9 +52,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// Add compression middleware
+app.use(compression());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/categories', require('./routes/category'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
