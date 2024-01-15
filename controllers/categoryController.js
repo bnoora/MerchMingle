173b50c4bf9f276c40c350b7c1a87559b0b4ab98
bnoora <bnooranii@gmail.com>
2024-01-14 17:45:34 -0800

    category create

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -23,16 +23,15 @@ exports.category_create_get = (req, res, next) => {
 };
 
 // Handle Category create on POST.
-exports.category_create_post = (req, res, next) => {
-    // Validate and sanitize fields.
-    body("name", "Category name required").trim().isLength({ min: 1 }).escape();
-    body("description", "Category description required").trim().isLength({ min: 1 }).escape();
+exports.category_create_post = [
+    body("name", "Category name required").trim().isLength({ min: 1 }).escape(),
+    body("description", "Category description required").trim().isLength({ min: 1 }).escape(),
 
     asyncHandler(async (req, res, next) => {
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
-            res.render("category_form", { title: "Create Category", category: category, errors: errors.array() });
+            res.render("category_form", { title: "Create Category", category: req.body, errors: errors.array() });
             return;
         } else {
             const foundCategory = await Category.findOne({ name: req.body.name });
@@ -40,11 +39,11 @@ exports.category_create_post = (req, res, next) => {
                 res.redirect(foundCategory.url);
             } else {
                 const newCat = await Category.create(req.body);
-                res.redirect(newCat.url);
+                res.redirect(newCat.url); // Ensure this URL is correctly generated in your Category model
             }
         }
-    });
-};
+    })
+];
 
 // Display Category delete form on GET.
 exports.category_delete_get = asyncHandler(async(req, res, next) => {
