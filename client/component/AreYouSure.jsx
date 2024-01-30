a753e701e28af23b4ae33db589bb19097ea074c2
bnoora <bnooranii@gmail.com>
2024-01-29 21:38:48 -0800

    fix: fixed routes and error handling and proper use of navigate

--- client/component/AreYouSure.jsx
+++ client/component/AreYouSure.jsx
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export default function AreYouSure(props) {
-    const { item, category } = props;
+    const { item, category, setShowSure } = props;
     const [showError, setShowError] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
     
 
     const deleteItem = () => {
-        setShowError(false);
-        axios.delete(`/api/${category ? 'category' : 'product'}/${item.id}`)
-        .then((response) => {
-            if (response.status === 200) {
-                props.setShowSure(false);
-                return <Navigate to={category ? '/categories' : '/products'}/>
-
-            }
-            else {
+        if (category === true) {
+            axios.delete(`/api/category/delete/${item}`)
+            .then((response) => {
+                setShowSure(false);
+                navigate('/categories');
+            })
+            .catch((error) => {
                 setShowError(true);
-                setErrorMessage(response.data.error);
-            }
+                setErrorMessage(error.response?.data?.error || "An error occurred.");
+            });
         }
-        )
+        else {
+            axios.delete(`/api/item/delete/${item}`)
+            .then((response) => {
+                setShowSure(false);
+                navigate('/products');
+            })
+            .catch((error) => {
+                setShowError(true);
+                setErrorMessage(error.response?.data?.error || "An error occurred.");
+            })
+        }
     }
+
+    useEffect(() => {
+    }, [showError, errorMessage]);
 
     return (
         <div>
