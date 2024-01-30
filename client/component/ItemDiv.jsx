ca6450922d851d9425064191c55319d316c84c9b
bnoora <bnooranii@gmail.com>
2024-01-29 21:39:11 -0800

    fix: fixed routes

--- client/component/ItemDiv.jsx
+++ client/component/ItemDiv.jsx
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom';
 export default function ItemDiv(props) {
     const { item, category } = props;
 
+
     return (
         <div className="ItemDiv">
             {category ? (
-                <Link to={`/categories/${item.id}`}>
+                <Link to={`/categories/${item._id}`}>
                     <h2>{item.name}</h2>
                 </Link>
             ) : (
-                <Link to={`/products/${item.id}`}>
+                <Link to={`/products/${item._id}`}>
                     <h2>{item.name}</h2>
                 </Link>
             )}
