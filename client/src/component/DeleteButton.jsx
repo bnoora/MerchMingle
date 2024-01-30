63d73433b36456b19f7cd68c9d7a6a4b25f53b25
bnoora <bnooranii@gmail.com>
2024-01-30 14:58:32 -0800

    style: delete and edit button

--- client/src/component/DeleteButton.jsx
+++ client/src/component/DeleteButton.jsx
@@ -2,7 +2,11 @@ export default function DeleteButton(props) {
     const {setShowSure} = props;
     return (
         <div>
-            <button onClick={() => setShowSure(true)}>Delete</button>
+            <button onClick={() => setShowSure(true)}
+            className='bg-red-500 hover:bg-red-600 rounded-2xl w-16 h-8 
+            flex flex-row items-center justify-center
+            transition-colors duration-200 hover:cursor-pointer
+            '>Delete</button>
         </div>
     )
 }
--- client/src/component/EditButton.jsx
+++ client/src/component/EditButton.jsx
@@ -2,7 +2,11 @@ export default function EditButton(props) {
     const {setShowEdit} = props;
     return (
         <div>
-            <button onClick={() => setShowEdit(true)}>Edit</button>
+            <button onClick={() => setShowEdit(true)}
+            className="bg-blue-500 hover:bg-blue-600 rounded-2xl 
+                        flex flex-row justify-center w-16 h-8 items-center
+                        transition-colors duration-200 hover:cursor-pointer"
+            >Edit</button>
         </div>
     )
 }
