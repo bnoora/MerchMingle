8c835133e224fce7692dbb335b15d46c01655c94
bnoora <bnooranii@gmail.com>
2024-01-30 13:06:08 -0800

    style: configured tailwind and postcss and added fonts

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Sixtyfour: ["Sixtyfour", "Sixtyfour"],
+        GloriaHallelujah : ["Gloria Hallelujah", "Gloria Hallelujah"],
+        Kalam: ["Kalam", "Kalam"],
+      },
+    },
+  },
+  plugins: [],
+}
+
